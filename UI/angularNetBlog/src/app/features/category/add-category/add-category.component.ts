153184b24cec72b708b65212b415f9b02593aa68
william-grullon <william-grullon@users.noreply.github.com>
2024-02-03 11:06:51 -0500

    Add CORS configuration and implement addCategory method in AddCategoryComponent

--- UI/angularNetBlog/src/app/features/category/add-category/add-category.component.ts
+++ UI/angularNetBlog/src/app/features/category/add-category/add-category.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-resquest.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription
 
-  constructor() {
+  constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -18,5 +21,18 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit() {
-  }
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
+      .subscribe({
+        next: (response) => {
+          console.log('Category added');
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+        }
+      });
+    };
+    
+    ngOnDestroy(): void {
+      this.addCategorySubscription?.unsubscribe();
+    }
 }
