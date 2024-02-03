9fb1790e02ba306ce7cd4ee813b18175376a1538
william-grullon <william-grullon@users.noreply.github.com>
2024-02-03 09:12:54 -0500

    Add category list component and update routing

--- UI/angularNetBlog/src/app/app.module.ts
+++ UI/angularNetBlog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
