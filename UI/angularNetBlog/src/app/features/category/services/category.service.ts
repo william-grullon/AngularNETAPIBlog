64d9df66b95309c54ecb83adfd25152f468fab4b
william-grullon <william-grullon@users.noreply.github.com>
2024-02-04 08:38:40 -0500

    Added the environment files and global API URLs

--- UI/angularNetBlog/src/app/features/category/services/category.service.ts
+++ UI/angularNetBlog/src/app/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { AddCategoryRequest } from '../models/add-category-resquest.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
   
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('http://localhost:5201/api/categories', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
   }
 
   getAllCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>('http://localhost:5201/api/categories');
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   }
 }
