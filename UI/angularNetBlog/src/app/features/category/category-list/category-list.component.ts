a066edab2b5baebcc5fff0c65f383e26f1549fd9
william-grullon <william-grullon@users.noreply.github.com>
2024-02-04 09:24:41 -0500

    Added redirection after adding a cat and switched the list to an observable

--- UI/angularNetBlog/src/app/features/category/category-list/category-list.component.ts
+++ UI/angularNetBlog/src/app/features/category/category-list/category-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
@@ -10,19 +11,11 @@ import { Category } from '../models/category.model';
 
 export class CategoryListComponent implements OnInit {
 
-  categories?: Category[];
+  categories$?: Observable<Category[]>;
 
-  constructor(private categoryService: CategoryService) { }
+ constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
-    this.categoryService.getAllCategories()
-      .subscribe({
-        next: (reponse) => {
-          this.categories = reponse;
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      });
+    this.categories$ = this.categoryService.getAllCategories();
   }
 }
