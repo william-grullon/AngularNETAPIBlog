e19d3d43a5e9160b1965dbca26b83389f0aa59bf
william-grullon <william-grullon@users.noreply.github.com>
2024-02-04 08:20:51 -0500

    Added getAllCategories method to CategoryService and models

--- UI/angularNetBlog/src/app/features/category/services/category.service.ts
+++ UI/angularNetBlog/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-resquest.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class CategoryService {
   
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>('http://localhost:5201/api/categories', model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>('http://localhost:5201/api/categories');
   }
 }
