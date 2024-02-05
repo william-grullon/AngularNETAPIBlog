698e29837712445c47f527ac591e23e1dbc88040
william-grullon <william-grullon@users.noreply.github.com>
2024-02-04 19:33:02 -0500

    Refactor edit-category component and added getCategoryById method to CategoryService

--- UI/angularNetBlog/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/angularNetBlog/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,15 +13,34 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
 
   id: string | null = null;
   paramsSubscription?: Subscription;
+  category?: Category;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService) {     
+  }
 
   ngOnInit(): void {
     this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if (this.id) {
+          // Fetch category by id
+          this.categoryService.getCategoryById(this.id).subscribe({
+            next: (reponse) => {
+              this.category = reponse;
+            },
+            error: (error) => {
+              console.error('Error fetching category by id', error);
+            }
+          });
+        }
       }
     });
+  }
+
+  onFormSubmit(): void {
+    console.log(this.category, 'Category updated successfully');
   }
 
   ngOnDestroy(): void {
